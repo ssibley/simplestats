f73abcefe4c54591a56dfff6a695234f041398e7
Dane Gellerup <danegellerup@gmail.com>
2015-01-16 11:19:03 -0600

    I added a median function.

--- stats.py
+++ stats.py
@@ -12,6 +12,13 @@ def mean(vals):
 
 def median(vals):
     """please implement this function"""
+	vals.sort()
+	z = len(vals)
+	index = z / 2
+	if z % 2 == 0:
+		return mean([vals[index], vals[index - 1]])
+    else:
+       return vals[index]
     pass
 
 def mode(vals):

5a353cee8a13fdc0898b7063e0485c28f5f3f9d6
Dane Gellerup <danegellerup@gmail.com>
2015-01-16 11:38:07 -0600

    Changed variable z to length

--- stats.py
+++ stats.py
@@ -13,9 +13,9 @@ def mean(vals):
 def median(vals):
     """please implement this function"""
 	vals.sort()
-	z = len(vals)
-	index = z / 2
-	if z % 2 == 0:
+	length = len(vals)
+	index = length / 2
+	if length % 2 == 0:
 		return mean([vals[index], vals[index - 1]])
     else:
        return vals[index]
